f3675bdba5a2ce3e38e9cd68192e5b4f9b7b8055
Abdul Saboor <abdulsaboor.bscssef20@iba-suk.edu.pk>
2025-05-18 19:14:16 +0500

    Dynamic task tiles implemented

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,44 +1,30 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function(bool?) onToggle;
 
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
-
-  void toggleCheckbox(bool? newValue) {
-    setState(() {
-      isChecked = newValue ?? false;
-    });
-  }
+  TaskTile({
+    required this.isChecked,
+    required this.taskTitle,
+    required this.onToggle,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        "This is task. ",
+        taskTitle,
         style: TextStyle(
           decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: TaskCheckBox(checkboxState: isChecked, toggleCheckbox: toggleCheckbox),
-    );
-  }
-}
-
-class TaskCheckBox extends StatelessWidget {
-  TaskCheckBox({required this.checkboxState, required this.toggleCheckbox});
-
-  final bool checkboxState;
-  final ValueChanged<bool?> toggleCheckbox;
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: checkboxState,
-      activeColor: Colors.lightBlueAccent,
-      onChanged: toggleCheckbox,
+      trailing: Checkbox(
+        value: isChecked,
+        activeColor: Colors.lightBlueAccent,
+        onChanged: onToggle,
+      ),
     );
   }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,11 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
+import 'package:todoey_flutter/model/task.dart';
+import 'package:flutter/material.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
   @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Do homework'),
+    Task(name: 'Clean the house'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(children: [TaskTile(), TaskTile(), TaskTile()]);
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          isChecked: tasks[index].isDone,
+          taskTitle: tasks[index].name,
+          onToggle: (newValue) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
+      itemCount: tasks.length,
+    );
   }
 }
